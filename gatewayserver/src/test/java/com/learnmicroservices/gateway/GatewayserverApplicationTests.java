87b7a35f2087cf68140916c058e0c1371b781c92
akhil <gollaakhil1115@gmail.com>
2024-06-19 12:21:56 -0500

    implemented spring- API-Gateway

---
+++ gatewayserver/src/test/java/com/learnmicroservices/gateway/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnmicroservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
